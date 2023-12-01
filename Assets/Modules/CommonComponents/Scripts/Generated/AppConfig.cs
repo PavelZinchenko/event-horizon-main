91d33e6c47a91ff8d43889afc36e1afbd79492a3
PavelZinchenko <zipas.games@gmail.com>
2023-12-01 11:09:11 +0400

    chore: update version and packages

--- Assets/Modules/CommonComponents/Scripts/Generated/AppConfig.cs
+++ Assets/Modules/CommonComponents/Scripts/Generated/AppConfig.cs
@@ -2,8 +2,8 @@ public static class AppConfig
 {
     public const string bundleIdentifier = "com.ZipasGames.EventHorizon";
     public const string version = "1.10.2";
-    public const int versionCode = 110200;
-    public const int buildNumber = 1462;
+    public const int versionCode = 110201;
+    public const int buildNumber = 1474;
     public const string localizationFile = "Languages";
     public const bool testMode = false;
     public const bool enableCheats = true;
