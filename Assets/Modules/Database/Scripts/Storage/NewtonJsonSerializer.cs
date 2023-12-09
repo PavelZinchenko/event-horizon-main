ab313f569a782a12203127210ec7b408a99c45dd
PavelZinchenko <zipas.games@gmail.com>
2023-12-09 11:09:53 +0400

    fix: make JsonSerializer ignore properties
    resolve #59

--- Assets/Modules/Database/Scripts/Storage/NewtonJsonSerializer.cs
+++ Assets/Modules/Database/Scripts/Storage/NewtonJsonSerializer.cs
@@ -1,14 +1,21 @@
-﻿using Newtonsoft.Json;
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Serialization;
 
 namespace GameDatabase.Storage
 {
     public class NewtonJsonSerializer : IJsonSerializer
     {
-        public NewtonJsonSerializer()
+		private readonly JsonSerializerSettings _settings;
+		
+		public NewtonJsonSerializer()
         {
             _settings = new JsonSerializerSettings
             {
-                Formatting = Formatting.Indented,
+				ContractResolver = new ContractResolver(),
+				Formatting = Formatting.Indented,
                 NullValueHandling = NullValueHandling.Ignore,
                 DefaultValueHandling = DefaultValueHandling.Ignore,
             };
@@ -24,6 +31,14 @@ namespace GameDatabase.Storage
             return JsonConvert.SerializeObject(item, _settings);
         }
 
-        private readonly JsonSerializerSettings _settings;
+		private class ContractResolver : DefaultContractResolver
+		{
+			protected override JsonProperty CreateProperty(MemberInfo member, MemberSerialization memberSerialization)
+			{
+				if (member.MemberType == MemberTypes.Property) return null;
+
+				return base.CreateProperty(member, memberSerialization);
+			}
+		}
     }
 }
