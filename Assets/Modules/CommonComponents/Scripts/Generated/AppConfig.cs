a3b89205db2ba1a020cd12c108f6afae98c5697a
PavelZinchenko <zipas.games@gmail.com>
2023-12-09 10:07:46 +0400

    chore: update version, remove empty folder

--- Assets/Modules/CommonComponents/Scripts/Generated/AppConfig.cs
+++ Assets/Modules/CommonComponents/Scripts/Generated/AppConfig.cs
@@ -2,8 +2,8 @@ public static class AppConfig
 {
     public const string bundleIdentifier = "com.ZipasGames.EventHorizon";
     public const string version = "1.10.2";
-    public const int versionCode = 110201;
-    public const int buildNumber = 1503;
+    public const int versionCode = 110203;
+    public const int buildNumber = 1509;
     public const string localizationFile = "Languages";
     public const bool testMode = false;
     public const bool enableCheats = true;
