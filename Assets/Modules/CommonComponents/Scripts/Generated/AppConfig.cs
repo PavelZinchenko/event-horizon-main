93f1dcafacedf6a52e14b5d3adcf779748a69ade
PavelZinchenko <zipas.games@gmail.com>
2023-12-14 17:11:01 +0400

    chore: update game version and iOS settings

--- Assets/Modules/CommonComponents/Scripts/Generated/AppConfig.cs
+++ Assets/Modules/CommonComponents/Scripts/Generated/AppConfig.cs
@@ -1,9 +1,9 @@
 public static class AppConfig
 {
     public const string bundleIdentifier = "com.ZipasGames.EventHorizon";
-    public const string version = "1.10.2";
-    public const int versionCode = 110203;
-    public const int buildNumber = 1509;
+    public const string version = "1.10.3";
+    public const int versionCode = 110302;
+    public const int buildNumber = 1533;
     public const string localizationFile = "Languages";
     public const bool testMode = false;
     public const bool enableCheats = true;
