9cf8a7a668c8af9a032f32e0bd58f1d7c8dfb1ef
PavelZinchenko <zipas.games@gmail.com>
2023-12-17 16:19:01 +0400

    test: add Database.Tests assembly

---
+++ Assets/Modules/Database/Tests/DatabaseTests.cs
@@ -0,0 +1,68 @@
+using System.Linq;
+using NUnit.Framework;
+using GameDatabase.Enums;
+using GameDatabase.Query;
+
+namespace GameDatabase.Tests
+{
+	[TestFixture]
+	public class DatabaseTests
+    {
+		private IDatabase Database { get; } = new Database();
+
+		[OneTimeSetUp]
+		public void RunBeforeAnyTests()
+		{
+			Database.LoadDefault();
+		}
+
+		[OneTimeTearDown]
+		public void RunAfterAnyTests()
+		{
+		}
+
+		[Test]
+		public void TestShipMinSpawnDistance()
+		{
+			var expression = Database.GalaxySettings.ShipMinSpawnDistance;
+			Assert.Greater(expression(SizeClass.Destroyer), expression(SizeClass.Frigate));
+			Assert.Greater(expression(SizeClass.Cruiser), expression(SizeClass.Destroyer));
+			Assert.Greater(expression(SizeClass.Battleship), expression(SizeClass.Cruiser));
+		}
+
+		[Test]
+		public void FindPlayerShips()
+		{
+			var playerShips = ShipBuildQuery.PlayerShips(Database);
+			Assert.IsTrue(playerShips.All.Any());
+		}
+
+		[Test]
+		[TestCase(0)]
+		[TestCase(25)]
+		[TestCase(50)]
+		[TestCase(100)]
+		[TestCase(300)]
+		[TestCase(500)]
+		[TestCase(1000)]
+		public void FindEnemyShips(int distance)
+		{
+			var count = ShipBuildQuery.EnemyShips(Database).CommonAndRare().FilterByStarDistance(distance).All.Count();
+			UnityEngine.Debug.Log($"Found {count} valid enemy ship builds on distance {distance}");
+			Assert.Greater(count, 0);
+		}
+
+		[Test]
+		public void CheckFactions()
+		{
+			foreach (var faction in Database.FactionList)
+			{
+				var factionShips = ShipBuildQuery.EnemyShips(Database).BelongToFaction(faction);
+				var ships = factionShips.CommonAndRare().All.Count();
+				var flagships = factionShips.Flagships().All.Count();
+				UnityEngine.Debug.Log($"Faction {faction.Name} has {ships} ships and {flagships} flagships");
+				Assert.IsTrue(ships + flagships > 0 || faction.NoTerritories && faction.NoWanderingShips);
+			}
+		}
+	}
+}
