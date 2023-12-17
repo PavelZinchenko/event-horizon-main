ce09eaf7e293db1b35b4b97f814604c6b9f8e030
PavelZinchenko <zipas.games@gmail.com>
2023-12-17 15:39:16 +0400

    chore: turn off alternative game title by default

--- Assets/Modules/CommonComponents/Scripts/Generated/AppConfig.cs
+++ Assets/Modules/CommonComponents/Scripts/Generated/AppConfig.cs
@@ -3,10 +3,10 @@ public static class AppConfig
     public const string bundleIdentifier = "com.ZipasGames.EventHorizon";
     public const string version = "1.10.3";
     public const int versionCode = 110302;
-    public const int buildNumber = 1533;
+    public const int buildNumber = 1536;
     public const string localizationFile = "Languages";
     public const bool testMode = false;
     public const bool enableCheats = true;
-    public const bool alternativeTitle = true;
+    public const bool alternativeTitle = false;
 }
 
