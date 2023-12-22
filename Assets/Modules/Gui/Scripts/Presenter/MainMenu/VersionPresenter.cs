69b6e04255b60a45cf5688b7131bf46ce0615547
PavelZinchenko <zipas.games@gmail.com>
2023-12-22 16:52:22 +0400

    refactor: rewrite game version text using UiToolkit

---
+++ Assets/Modules/Gui/Scripts/Presenter/MainMenu/VersionPresenter.cs
@@ -0,0 +1,10 @@
+﻿namespace Gui.Presenter.MainMenu
+{
+    public partial class VersionPresenter : PresenterBase
+    {
+        private void Start()
+        {
+			Version.text = AppConfig.version + " (build " + AppConfig.buildNumber + ")";
+		}
+	}
+}
