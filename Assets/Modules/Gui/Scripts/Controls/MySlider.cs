fb92e94c3a7fc3fa5f538d042b235176a9e1b0ed
PavelZinchenko <zipas.games@gmail.com>
2023-12-25 14:22:56 +0400

    fix(ui): fix visual bug in Unity Slider control

---
+++ Assets/Modules/Gui/Scripts/Controls/MySlider.cs
@@ -0,0 +1,38 @@
+﻿namespace UnityEngine.UIElements
+{
+	public class MySlider : Slider
+	{
+		public MySlider()
+		{
+			_dragger = this.Q(null, draggerUssClassName);
+			_dragContainer = this.Q(null, trackerUssClassName);
+		}
+
+		private VisualElement _dragger;
+		private VisualElement _dragContainer;
+
+		public override void HandleEvent(EventBase evt)
+		{
+			base.HandleEvent(evt);
+			if (evt is IPointerEvent || evt is IMouseEvent)
+				AdjustPosition();
+		}
+
+		private void AdjustPosition()
+		{
+			var width = _dragContainer.contentRect.width - _dragger.contentRect.width;
+			var position = _dragger.transform.position;
+
+			if (position.x < 0)
+				position.x = 0;
+			else if (position.x > width)
+				position.x = width;
+			else
+				return;
+				
+			_dragger.transform.position = position;
+		}
+
+		public new class UxmlFactory : UxmlFactory<MySlider, UxmlTraits> { }
+	}
+}
