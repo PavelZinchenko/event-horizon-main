f576c45b0fcdceb0dd11ead02b4942a74d87f35d
PavelZinchenko <zipas.games@gmail.com>
2024-01-23 17:59:21 +0400

    fix: resolve decorator node content deserialization issue

--- Assets/Modules/Database/Scripts/Storage/UnityJsonSerializer.cs
+++ Assets/Modules/Database/Scripts/Storage/UnityJsonSerializer.cs
@@ -4,8 +4,14 @@ namespace GameDatabase.Storage
 {
     public class UnityJsonSerializer : IJsonSerializer
     {
+		private NewtonJsonSerializer _newtonJsonSerializer;
+
         public T FromJson<T>(string data)
         {
+			// Temporary workaround: Unity JsonUtility seems to have troubles deserializing recursive classes, using NewtonJson instead
+			if (typeof(T) == typeof(Serializable.BehaviorTreeNodeSerializable))
+				return (_newtonJsonSerializer ??= new()).FromJson<T>(data);
+
             return JsonUtility.FromJson<T>(data);
         }
 
