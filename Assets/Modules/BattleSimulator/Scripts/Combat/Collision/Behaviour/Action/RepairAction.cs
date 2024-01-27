ed256b3381bc0dcfc38f93287fadc619a25870a8
PavelZinchenko <zipas.games@gmail.com>
2024-01-27 14:09:24 +0400

    feat: make repair ammo repair allies and deal Power*Factor damage to ememies

--- Assets/Modules/BattleSimulator/Scripts/Combat/Collision/Behaviour/Action/RepairAction.cs
+++ Assets/Modules/BattleSimulator/Scripts/Combat/Collision/Behaviour/Action/RepairAction.cs
@@ -1,14 +1,17 @@
 ﻿using Combat.Collision.Manager;
 using Combat.Component.Unit;
+using Combat.Component.Unit.Classification;
 using GameDatabase.Enums;
 
 namespace Combat.Collision.Behaviour.Action
 {
     public class RepairAction : ICollisionAction
     {
-        public RepairAction(float power, BulletImpactType impactType)
+        public RepairAction(float power, BulletImpactType impactType, DamageType damageType, float damageMultiplier)
         {
             _power = power;
+			_damageMultiplier = damageMultiplier > 0 ? damageMultiplier : 1;
+			_damageType = damageType;
             _impactType = impactType;
         }
 
@@ -16,22 +19,31 @@ namespace Combat.Collision.Behaviour.Action
         {
             if (_impactType == BulletImpactType.DamageOverTime)
             {
-                targetImpact.Repair += _power * collisionData.TimeInterval;
-            }
-            else
+				if (self.Type.Side.IsAlly(target.Type.Side))
+	                targetImpact.Repair += _power * collisionData.TimeInterval;
+				else
+					targetImpact.AddDamage(_damageType, _power * collisionData.TimeInterval * _damageMultiplier);
+			}
+			else
             {
                 if (!collisionData.IsNew || !_isAlive)
                     return;
 
-                targetImpact.Repair += _power;
-                _isAlive = _impactType == BulletImpactType.HitAllTargets;
+				if (self.Type.Side.IsAlly(target.Type.Side))
+					targetImpact.Repair += _power;
+				else
+					targetImpact.AddDamage(_damageType, _power * _damageMultiplier);
+                
+				_isAlive = _impactType == BulletImpactType.HitAllTargets;
             }
         }
 
         public void Dispose() { }
 
         private bool _isAlive = true;
+		private readonly float _damageMultiplier;
         private readonly float _power;
+		private readonly DamageType _damageType;
         private readonly BulletImpactType _impactType;
     }
 }
