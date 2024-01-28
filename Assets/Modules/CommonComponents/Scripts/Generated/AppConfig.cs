4b9428057b30f7fb2b3b7a5ea5a0b13ab280d56a
PavelZinchenko <zipas.games@gmail.com>
2024-01-28 10:18:34 +0400

    chore: update .gitignore, game version and Editor submodule

--- Assets/Modules/CommonComponents/Scripts/Generated/AppConfig.cs
+++ Assets/Modules/CommonComponents/Scripts/Generated/AppConfig.cs
@@ -3,7 +3,7 @@ public static class AppConfig
     public const string bundleIdentifier = "com.ZipasGames.EventHorizon";
     public const string version = "1.10.4";
     public const int versionCode = 110400;
-    public const int buildNumber = 1591;
+    public const int buildNumber = 1597;
     public const string localizationFile = "Languages";
     public const bool testMode = false;
     public const bool enableCheats = true;
