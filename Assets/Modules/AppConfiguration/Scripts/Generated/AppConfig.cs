7a34d6539079808679b42ca47c8e7b636e41a50e
PavelZinchenko <zipas.games@gmail.com>
2024-02-15 11:15:38 +0400

    chore: update game version, turn on debug console

--- Assets/Modules/AppConfiguration/Scripts/Generated/AppConfig.cs
+++ Assets/Modules/AppConfiguration/Scripts/Generated/AppConfig.cs
@@ -1,9 +1,9 @@
 public static class AppConfig
 {
     public const string bundleIdentifier = "com.ZipasGames.EventHorizon";
-    public const string version = "1.10.4";
-    public const int versionCode = 110400;
-    public const int buildNumber = 1629;
+    public const string version = "1.11.0";
+    public const int versionCode = 111000;
+    public const int buildNumber = 1639;
     public const string localizationFile = "Languages";
     public const bool testMode = false;
     public const bool enableCheats = true;
