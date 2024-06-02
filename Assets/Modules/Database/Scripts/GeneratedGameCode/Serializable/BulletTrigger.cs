d6733a3e8f5177d599fcd26a1f7ef6648c03f873
PavelZinchenko <zipas.games@gmail.com>
2024-06-02 16:02:52 +0400

    fix: fix rotation formula bug
    resolve #342

--- Assets/Modules/Database/Scripts/GeneratedGameCode/Serializable/BulletTrigger.cs
+++ Assets/Modules/Database/Scripts/GeneratedGameCode/Serializable/BulletTrigger.cs
@@ -31,8 +31,8 @@ namespace GameDatabase.Serializable
 		public int MaxNestingLevel;
 		public bool OncePerCollision;
 		public bool UseBulletPosition;
-		public string Rotation = "IF(Quantity == 1, 0, RANDOM(0, 360))";
-		public string OffsetX = "IF(Quantity == 1, 0, Size / 2)";
+		public string Rotation = "IF(Quantity <= 1, 0, RANDOM(0, 360))";
+		public string OffsetX = "IF(Quantity <= 1, 0, Size / 2)";
 		public string OffsetY = "0";
 	}
 }
