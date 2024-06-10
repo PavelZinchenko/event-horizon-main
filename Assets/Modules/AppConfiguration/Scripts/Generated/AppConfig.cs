c80ec988720717cae8f55ecc2c14457e2a4f8d79
PavelZinchenko <zipas.games@gmail.com>
2024-06-10 14:36:15 +0400

    feat: upgrade project to Unity 2022.3

--- Assets/Modules/AppConfiguration/Scripts/Generated/AppConfig.cs
+++ Assets/Modules/AppConfiguration/Scripts/Generated/AppConfig.cs
@@ -1,9 +1,9 @@
 public static class AppConfig
 {
     public const string bundleIdentifier = "com.ZipasGames.EventHorizon";
-    public const string version = "1.11.1";
-    public const int versionCode = 111100;
-    public const int buildNumber = 1655;
+    public const string version = "1.12.1";
+    public const int versionCode = 112100;
+    public const int buildNumber = 1706;
     public const string localizationFile = "Languages";
     public const bool testMode = false;
     public const bool enableCheats = true;
