ab9d846ddd0d38cdc303a2117a0b8d904cc724b7
Tony Li <domehero@outlook.com>
2026-02-07 06:55:19 +0800

    Updated Unity-Editor Version & Unity Packages Version

--- Assets/Modules/AppConfiguration/Scripts/Generated/AppConfig.cs
+++ Assets/Modules/AppConfiguration/Scripts/Generated/AppConfig.cs
@@ -1,9 +1,9 @@
 public static class AppConfig
 {
-    public const string bundleIdentifier = "com.ZipasGames.EventHorizon";
+    public const string bundleIdentifier = "unity.ZipasGames.Event Horizon";
     public const string version = "1.12.1";
     public const int versionCode = 112100;
-    public const int buildNumber = 1706;
+    public const int buildNumber = 1707;
     public const string localizationFile = "Languages";
     public const bool testMode = false;
     public const bool enableCheats = true;
